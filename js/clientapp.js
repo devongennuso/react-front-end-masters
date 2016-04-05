2ba2a362532ce2e795a6717837b22222b84b3eff
Devon Gennuso <devon.gennuso@gmail.com>
2016-04-05 11:43:56 -0500

    listing using standard

--- js/clientapp.js
+++ js/clientapp.js
@@ -1,31 +1,31 @@
+/* global React ReactDOM */
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-  render() {
+  render () {
     return (
       div(null,
         h1({style: {color: this.props.color}}, this.props.title)
       )
     )
   }
-});
+})
 
-var MyTitleFactory = React.createFactory(MyTitle);
-var ce = React.createElement;
+var MyTitleFactory = React.createFactory(MyTitle)
 
 // composite components
 var MyFirstCompononet = (
   div(null,
     MyTitleFactory({
-      title: "Props are great",
-      color: "rebeccapurple"}),
+      title: 'Props are great',
+      color: 'rebeccapurple'}),
     MyTitleFactory({
-      title: "Use props everywhere!",
-      color: "mediumaquamarine"}),
+      title: 'Use props everywhere!',
+      color: 'mediumaquamarine'}),
     MyTitleFactory({
-      title: "Props are the best!",
-      color: "peru"
+      title: 'Props are the best!',
+      color: 'peru'
     })
   )
 )
